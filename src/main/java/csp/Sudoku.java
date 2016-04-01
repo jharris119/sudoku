3484a5098a04c44434bfd07cd0ebd13c15df6d05
Jay Harris <jayharris@gmail.com>
2016-04-01 13:01:03 -0400

    wrote sudoku solver and specs

---
+++ src/main/java/csp/Sudoku.java
@@ -0,0 +1,252 @@
+package csp;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import com.google.common.primitives.Ints;
+
+import java.util.*;
+
+public class Sudoku {
+
+    Set<Candidate> givens;
+    Set<Candidate> solution = null;
+    final int size, boxesPerRow, boxesPerColumn;
+
+    public Sudoku(Set<Candidate> givens) {
+        this.givens = givens;
+
+        this.size = getSize();
+        this.boxesPerColumn = (int) (Math.sqrt(this.size));
+        this.boxesPerRow = (int) (Math.sqrt(this.size));
+    }
+
+    public Set<Candidate> solve() {
+        if (solution != null) {
+            return solution;
+        }
+
+        return solution = new ExactCoverProblem<Candidate, Constraint>(makeCandidates(), makeConstraints()) {
+            @Override
+            public boolean relation(Constraint constraint, Candidate candidate) {
+                return constraint.isSatisfiedBy(candidate);
+            }
+        }.solve();
+    }
+
+    public void print() {
+        if (solution == null) {
+            solve();
+        }
+
+        if (solution == null) {
+            System.out.println("No solution.");
+            return;
+        }
+
+        int cellsize = (int) Math.ceil(Math.log10(size));
+
+        int[][] asGrid = new int[size][size];
+        solution.forEach(candidate -> asGrid[candidate.row - 1][candidate.column - 1] = candidate.digit);
+
+        StringBuilder sb = new StringBuilder(topOrBottomRow(cellsize)).append("\n");
+        for (int row = 0; row < size; ++row) {
+            sb.append(rowOfDigits(asGrid[row], cellsize)).append("\n");
+            if (row % boxesPerColumn == boxesPerColumn - 1 && row + 1 != size) {
+                sb.append(separatorRow(cellsize)).append("\n");
+            }
+        }
+        sb.append(topOrBottomRow(cellsize));
+        System.out.println(sb.toString());
+    }
+
+    public String rowOfDigits(int[] row, int cellWidth) {
+        StringBuilder sb = new StringBuilder("|");
+        StringJoiner joiner;
+        List<List<Integer>> partitions = Lists.partition(Ints.asList(row), boxesPerRow);
+
+        for (List<Integer> sublist : partitions) {
+            joiner = new StringJoiner(" ", "", "|");
+            for (Integer i : sublist) {
+                joiner.add(String.format("%" + cellWidth + "d", i));
+            }
+            sb.append(joiner.toString());
+        }
+        return sb.toString();
+    }
+
+    public String separatorRow(int cellWidth) {
+        StringBuilder sb = new StringBuilder("|");
+        for (int i = 0; i < boxesPerRow; ++i) {
+            sb.append(Strings.repeat("-", (cellWidth + 1) * boxesPerColumn - 1)).append("+");
+        }
+        return sb.replace(sb.length() - 1, sb.length(), "|").toString();
+    }
+
+    public String topOrBottomRow(int cellWidth) {
+        return Strings.repeat("-", ((cellWidth + 1) * boxesPerColumn) * boxesPerRow + 1);
+    }
+
+
+    private Set<Constraint> makeConstraints() {
+        Set<Constraint> constraints = new HashSet();
+        for (int m = 1; m <= size; ++m) {
+            for (int n = 1; n <= size; ++n) {
+                constraints.add(new RowColumnConstraint(m, n));
+                constraints.add(new RowDigitConstraint(m, n));
+                constraints.add(new ColumnDigitConstraint(m, n));
+                constraints.add(new BoxDigitConstraint(m, n));
+            }
+        }
+        return constraints;
+    }
+
+    private Set<Candidate> makeCandidates() {
+        Set<Candidate> candidates = new HashSet();
+
+        for (int row = 1; row <= size; ++row) {
+            for (int column = 1; column <= size; ++column) {
+                if (isGiven(row, column)) {
+                    candidates.add(getGiven(row, column).get());
+                }
+                else {
+                    for (int digit = 1; digit <= size; ++digit) {
+                        if (givenDigitInRow(row, digit)) { continue; }
+                        if (givenDigitInColumn(column, digit)) { continue; }
+                        if (givenDigitInBox(getBox(row, column), digit)) { continue; }
+                        candidates.add(new Candidate(row, column, digit));
+                    }
+                }
+            }
+        }
+        return candidates;
+    }
+
+    public boolean isGiven(int row, int column) {
+        return givens.stream().anyMatch(given -> given.row == row && given.column == column);
+    }
+
+    public Optional<Candidate> getGiven(int row, int column) {
+        return givens.stream().filter(given -> given.row == row && given.column == column).findAny();
+    }
+
+    public boolean givenDigitInRow(int row, int digit) {
+        return givens.stream().anyMatch(given -> given.row == row && given.digit == digit);
+    }
+
+    public boolean givenDigitInColumn(int column, int digit) {
+        return givens.stream().anyMatch(given -> given.column == column && given.digit == digit);
+    }
+
+    public boolean givenDigitInBox(int box, int digit) {
+        return givens.stream().anyMatch(given -> getBox(given) == box && given.digit == digit);
+    }
+
+    protected int getBox(Candidate candidate) {
+        return getBox(candidate.row, candidate.column);
+    }
+
+    protected int getBox(int row, int column) {
+        return (ceilDiv(row, boxesPerColumn) - 1) * boxesPerColumn + ceilDiv(column, boxesPerRow);
+    }
+
+    private int getSize() {
+        return givens.stream().mapToInt(candidate -> {
+            int m = Math.max(candidate.row, candidate.column);
+            return Math.max(m, candidate.digit);
+        }).max().getAsInt();
+    }
+
+    private int ceilDiv(int x, int y) {
+        return (x + y - 1) / y;
+    }
+
+    static class Candidate {
+        final int row, column, digit;
+
+        public Candidate(int row, int column, int digit) {
+            this.row = row;
+            this.column = column;
+            this.digit = digit;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Candidate candidate = (Candidate) o;
+            return row == candidate.row &&
+                    column == candidate.column &&
+                    digit == candidate.digit;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(row, column, digit);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("[%d,%d,%d]", row, column, digit);
+        }
+    }
+
+    abstract class Constraint {
+        public abstract boolean isSatisfiedBy(Candidate candidate);
+    }
+
+    class RowColumnConstraint extends Constraint {
+        final int row, column;
+
+        RowColumnConstraint(int row, int column) {
+            this.row = row;
+            this.column = column;
+        }
+
+        @Override
+        public boolean isSatisfiedBy(Candidate candidate) {
+            return candidate.row == this.row && candidate.column == this.column;
+        }
+    }
+
+    class RowDigitConstraint extends Constraint {
+        final int row, digit;
+
+        public RowDigitConstraint(int row, int digit) {
+            this.row = row;
+            this.digit = digit;
+        }
+
+        @Override
+        public boolean isSatisfiedBy(Candidate candidate) {
+            return candidate.row == this.row && candidate.digit == this.digit;
+        }
+    }
+
+    class ColumnDigitConstraint extends Constraint {
+        final int column, digit;
+
+        public ColumnDigitConstraint(int column, int digit) {
+            this.column = column;
+            this.digit = digit;
+        }
+
+        @Override
+        public boolean isSatisfiedBy(Candidate candidate) {
+            return candidate.column == this.column && candidate.digit == this.digit;
+        }
+    }
+
+    class BoxDigitConstraint extends Constraint {
+        final int box, digit;
+
+        public BoxDigitConstraint(int box, int digit) {
+            this.box = box;
+            this.digit = digit;
+        }
+
+        @Override
+        public boolean isSatisfiedBy(Candidate candidate) {
+            return getBox(candidate.row, candidate.column) == this.box && candidate.digit == this.digit;
+        }
+    }
+}
---
+++ src/test/java/csp/SudokuTest.java
@@ -0,0 +1,243 @@
+package csp;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+
+public class SudokuTest {
+
+    public Set<Sudoku.Candidate> puzzle9x9 = new HashSet<Sudoku.Candidate>() {{
+        this.add(new Sudoku.Candidate(1,3,6));
+        this.add(new Sudoku.Candidate(1,4,9));
+        this.add(new Sudoku.Candidate(1,8,8));
+        this.add(new Sudoku.Candidate(1,9,7));
+        this.add(new Sudoku.Candidate(2,5,1));
+        this.add(new Sudoku.Candidate(2,6,6));
+        this.add(new Sudoku.Candidate(3,3,7));
+        this.add(new Sudoku.Candidate(3,5,3));
+        this.add(new Sudoku.Candidate(3,8,9));
+        this.add(new Sudoku.Candidate(4,1,6));
+        this.add(new Sudoku.Candidate(4,2,5));
+        this.add(new Sudoku.Candidate(4,7,8));
+        this.add(new Sudoku.Candidate(4,9,4));
+        this.add(new Sudoku.Candidate(5,4,6));
+        this.add(new Sudoku.Candidate(5,8,1));
+        this.add(new Sudoku.Candidate(6,7,7));
+        this.add(new Sudoku.Candidate(7,5,2));
+        this.add(new Sudoku.Candidate(7,6,8));
+        this.add(new Sudoku.Candidate(7,8,3));
+        this.add(new Sudoku.Candidate(7,9,9));
+        this.add(new Sudoku.Candidate(8,1,9));
+        this.add(new Sudoku.Candidate(8,3,3));
+        this.add(new Sudoku.Candidate(8,4,1));
+        this.add(new Sudoku.Candidate(9,2,2));
+        this.add(new Sudoku.Candidate(9,3,5));
+    }};
+
+    public Set<Sudoku.Candidate> puzzle16x16 = new HashSet<Sudoku.Candidate>() {{
+        this.add(new Sudoku.Candidate(1,1,6));
+        this.add(new Sudoku.Candidate(1,6,13));
+        this.add(new Sudoku.Candidate(1,9,8));
+        this.add(new Sudoku.Candidate(1,13,11));
+        this.add(new Sudoku.Candidate(1,14,2));
+        this.add(new Sudoku.Candidate(2,1,10));
+        this.add(new Sudoku.Candidate(2,2,13));
+        this.add(new Sudoku.Candidate(2,4,3));
+        this.add(new Sudoku.Candidate(2,6,12));
+        this.add(new Sudoku.Candidate(2,11,11));
+        this.add(new Sudoku.Candidate(2,12,15));
+        this.add(new Sudoku.Candidate(2,13,6));
+        this.add(new Sudoku.Candidate(2,14,4));
+        this.add(new Sudoku.Candidate(3,4,7));
+        this.add(new Sudoku.Candidate(3,7,6));
+        this.add(new Sudoku.Candidate(3,8,4));
+        this.add(new Sudoku.Candidate(3,9,12));
+        this.add(new Sudoku.Candidate(3,12,10));
+        this.add(new Sudoku.Candidate(3,16,8));
+        this.add(new Sudoku.Candidate(4,1,9));
+        this.add(new Sudoku.Candidate(4,2,14));
+        this.add(new Sudoku.Candidate(4,3,11));
+        this.add(new Sudoku.Candidate(4,4,15));
+        this.add(new Sudoku.Candidate(4,7,16));
+        this.add(new Sudoku.Candidate(4,12,5));
+        this.add(new Sudoku.Candidate(4,15,1));
+        this.add(new Sudoku.Candidate(5,1,15));
+        this.add(new Sudoku.Candidate(5,2,1));
+        this.add(new Sudoku.Candidate(5,10,4));
+        this.add(new Sudoku.Candidate(5,12,8));
+        this.add(new Sudoku.Candidate(5,15,2));
+        this.add(new Sudoku.Candidate(5,16,10));
+        this.add(new Sudoku.Candidate(6,1,8));
+        this.add(new Sudoku.Candidate(6,2,5));
+        this.add(new Sudoku.Candidate(6,3,13));
+        this.add(new Sudoku.Candidate(6,11,7));
+        this.add(new Sudoku.Candidate(6,13,4));
+        this.add(new Sudoku.Candidate(7,5,4));
+        this.add(new Sudoku.Candidate(7,9,16));
+        this.add(new Sudoku.Candidate(7,10,5));
+        this.add(new Sudoku.Candidate(7,13,1));
+        this.add(new Sudoku.Candidate(7,14,8));
+        this.add(new Sudoku.Candidate(8,2,4));
+        this.add(new Sudoku.Candidate(8,3,10));
+        this.add(new Sudoku.Candidate(8,9,13));
+        this.add(new Sudoku.Candidate(8,11,1));
+        this.add(new Sudoku.Candidate(8,12,15));
+        this.add(new Sudoku.Candidate(8,13,3));
+        this.add(new Sudoku.Candidate(9,4,8));
+        this.add(new Sudoku.Candidate(9,8,12));
+        this.add(new Sudoku.Candidate(9,10,1));
+        this.add(new Sudoku.Candidate(9,16,13));
+        this.add(new Sudoku.Candidate(10,1,12));
+        this.add(new Sudoku.Candidate(10,5,3));
+        this.add(new Sudoku.Candidate(10,7,10));
+        this.add(new Sudoku.Candidate(10,15,16));
+        this.add(new Sudoku.Candidate(10,16,2));
+        this.add(new Sudoku.Candidate(11,6,16));
+        this.add(new Sudoku.Candidate(11,7,15));
+        this.add(new Sudoku.Candidate(11,9,7));
+        this.add(new Sudoku.Candidate(11,11,3));
+        this.add(new Sudoku.Candidate(11,14,10));
+        this.add(new Sudoku.Candidate(11,15,5));
+        this.add(new Sudoku.Candidate(12,1,4));
+        this.add(new Sudoku.Candidate(12,3,2));
+        this.add(new Sudoku.Candidate(12,6,1));
+        this.add(new Sudoku.Candidate(12,7,7));
+        this.add(new Sudoku.Candidate(12,8,5));
+        this.add(new Sudoku.Candidate(12,10,12));
+        this.add(new Sudoku.Candidate(12,11,13));
+        this.add(new Sudoku.Candidate(12,15,8));
+        this.add(new Sudoku.Candidate(13,1,13));
+        this.add(new Sudoku.Candidate(13,2,8));
+        this.add(new Sudoku.Candidate(13,4,15));
+        this.add(new Sudoku.Candidate(13,9,3));
+        this.add(new Sudoku.Candidate(13,11,9));
+        this.add(new Sudoku.Candidate(14,2,12));
+        this.add(new Sudoku.Candidate(14,3,14));
+        this.add(new Sudoku.Candidate(14,4,5));
+        this.add(new Sudoku.Candidate(14,6,10));
+        this.add(new Sudoku.Candidate(14,7,1));
+        this.add(new Sudoku.Candidate(15,1,1));
+        this.add(new Sudoku.Candidate(15,3,4));
+        this.add(new Sudoku.Candidate(13,5,7));
+        this.add(new Sudoku.Candidate(15,8,9));
+        this.add(new Sudoku.Candidate(15,9,10));
+        this.add(new Sudoku.Candidate(15,10,2));
+        this.add(new Sudoku.Candidate(15,12,6));
+        this.add(new Sudoku.Candidate(15,16,14));
+        this.add(new Sudoku.Candidate(16,4,9));
+        this.add(new Sudoku.Candidate(16,5,14));
+        this.add(new Sudoku.Candidate(16,7,4));
+        this.add(new Sudoku.Candidate(16,8,6));
+        this.add(new Sudoku.Candidate(16,9,15));
+        this.add(new Sudoku.Candidate(16,12,11));
+    }};
+
+    public Sudoku sudoku1, sudoku2;
+
+    @Before
+    public void setUp() throws Exception {
+        sudoku1 = new Sudoku(puzzle9x9);
+        sudoku2 = new Sudoku(puzzle16x16);
+    }
+
+    @Test
+    public void testIsGiven() throws Exception {
+        assertTrue(sudoku1.isGiven(3,8));
+        assertFalse(sudoku1.isGiven(8,7));
+    }
+
+    @Test
+    public void testGetGiven() throws Exception {
+        assertEquals(Optional.of(new Sudoku.Candidate(5,8,1)), sudoku1.getGiven(5,8));
+        assertEquals(Optional.empty(), sudoku1.getGiven(3,4));
+    }
+
+    @Test
+    public void testGivenDigitInRow() throws Exception {
+        assertTrue(sudoku1.givenDigitInRow(1,9));
+        assertFalse(sudoku1.givenDigitInRow(2,2));
+    }
+
+    @Test
+    public void testGivenDigitInColumn() throws Exception {
+        assertTrue(sudoku1.givenDigitInColumn(1,6));
+        assertFalse(sudoku1.givenDigitInColumn(9,8));
+    }
+
+    @Test
+    public void testGivenDigitInBox() throws Exception {
+        assertTrue(sudoku1.givenDigitInBox(6,7));
+        assertFalse(sudoku1.givenDigitInBox(7,4));
+    }
+
+    @Test
+    public void testSolve() throws Exception {
+        sudoku1.solve();
+        for (int i = 1; i <= sudoku1.size; ++i) {
+            final int row = i;
+            assertEquals(sudoku1.size, sudoku1.solution.stream()
+                    .filter(candidate -> candidate.row == row)
+                    .map(candidate -> candidate.digit)
+                    .collect(Collectors.toSet())
+                    .size()
+            );
+        }
+        for (int i = 1; i <= sudoku1.size; ++i) {
+            final int column = i;
+            assertEquals(sudoku1.size, sudoku1.solution.stream()
+                    .filter(candidate -> candidate.column == column)
+                    .map(candidate -> candidate.digit)
+                    .collect(Collectors.toSet())
+                    .size()
+            );
+        }
+        for (int i = 1; i <= sudoku1.size; ++i) {
+            final int box = i;
+            assertEquals(sudoku1.size, sudoku1.solution.stream()
+                    .filter(candidate -> sudoku1.getBox(candidate) == box)
+                    .map(candidate -> candidate.digit)
+                    .collect(Collectors.toSet())
+                    .size()
+            );
+        }
+
+        sudoku1.print();
+
+
+//        sudoku2.solve();
+//
+//        System.err.println(sudoku2.solution);
+//        assertTrue(false);
+    }
+
+    public Set<Integer> getDigitsInRow(Sudoku sudoku, int row) {
+        return sudoku.solution.stream()
+                .filter(candidate -> candidate.row == row)
+                .map(candidate -> candidate.digit)
+                .collect(Collectors.toSet());
+    }
+
+    @Test
+    public void testPrintRowOfDigits() throws Exception {
+        int[] digits = new int[] { 6, 8, 14, 3, 5, 10, 1, 11, 9, 2, 4, 7, 12, 15, 16, 13 };
+        assertEquals("| 6  8 14  3| 5 10  1 11| 9  2  4  7|12 15 16 13|", sudoku2.rowOfDigits(digits, 2));
+    }
+
+    @Test
+    public void testPrintSeparatorRow() throws Exception {
+        assertEquals("|-----------+-----------+-----------+-----------|", sudoku2.separatorRow(2));
+    }
+
+    @Test
+    public void testPrintTopOrBottomRow() throws Exception {
+        assertEquals("-------------------------------------------------", sudoku2.topOrBottomRow(2));
+    }
+}
