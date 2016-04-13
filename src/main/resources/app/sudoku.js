e89413956514b55db484261a250c919251a3b058
Jay Harris <jayharris@gmail.com>
2016-04-13 18:09:16 -0400

    update sudoku grid when a number is typed into a cell

--- src/main/resources/app/sudoku.js
+++ src/main/resources/app/sudoku.js
@@ -7,30 +7,51 @@ export default class Sudoku extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      givens: {},
       solved: false
     }
+    _.bindAll(this, 'onUpdateCell');
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('Sudoku.componentWillUpdate: %O, %O', nextProps, nextState);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('Sudoku.componentDidUpdate: %O, %O', prevProps, prevState);
+  }
+
+  onUpdateCell(row, column, digit) {
+    console.log('Sudoku.onUpdateCell');
+
+    let key = `[${row},${column}]`,
+        newGivens = Object.assign({}, this.state.givens);
+
+    if (digit) {
+      newGivens[key] = digit;
+    }
+    else {
+      delete newGivens[key];
+    }
+    this.setState({
+      givens: newGivens
+    });
   }
 
   render() {
-    let {
-      boxesPerRow: cellsPerBoxColumn,
-      boxesPerColumn: cellsPerBoxRow
-    } = this.props;
-    let size = cellsPerBoxRow * cellsPerBoxColumn;
+    console.log('Sudoku.render');
 
     return (
       <div id="sudoku">
         {
-          _.range(size).map((r) => {
+          _.range(this.props.size).map((r) => {
             return (
               <div id={`row${r}`}>{
-                _.range(size).map((c) => {
+                _.range(this.props.size).map((c) => {
                   return (
                     <SudokuCell row={r + 1}
                                 column={c + 1}
-                                boxRight={(c + 1) % cellsPerBoxRow == 0}
-                                boxBottom={(r + 1) % cellsPerBoxColumn == 0}
-                                size={size} />
+                                onUpdateCell={this.onUpdateCell} />
                   );
                 })}
               </div>
--- src/main/resources/app/sudokucell.js
+++ src/main/resources/app/sudokucell.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import $ from 'jquery';
+import _ from 'lodash';
 
 export default class SudokuCell extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      digit: null
+    }
+    _.bindAll(this, 'onChange');
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    if (this.state.digit != nextState.digit) {
+      this.props.onUpdateCell(nextProps.row, nextProps.column, nextState.digit);
+    }
+  }
+
+  onChange(evt) {
+    this.setState({
+      digit: evt.target.value
+    });
   }
 
   render() {
     return (
       <div className="cell">
-        <input style={{width: '100%', height: '100%', border: 'none'}} id={`cell[${this.props.row},${this.props.column}]`} type="text" />
+        <input style={{width: '100%', height: '100%', border: 'none'}}
+               id={`cell[${this.props.row},${this.props.column}]`}
+               type="text"
+               onChange={this.onChange} />
       </div>
     );
   }
